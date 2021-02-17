7fdf4529982a2f885e47d6b2cf12b605f246bae5
Gabrielcofs <79209098+Gabrielcofs@users.noreply.github.com>
2021-02-17 11:45:03 -0300

    help.js
    ...

--- src/help.js
+++ src/help.js
@@ -1,311 +1,199 @@
-const help = (prefix) => {
-	return `         *NABUTO LS O BRABO*
-
- INFO:✅
-
- *DONO* : NABUTO LS
- PARCEIRIAS: GUSTAVO, ITACHI DO HYPE
-
- *WHATSAPPS DOS PROPRIETÁRIOS* :
+INFO: ✅
+ * DONO *: GABRIEL COFS
+ PARCEIRIAS: CARLOS GAY
+ * WHATSAPPS DOS PROPRIETÁRIOS *:
  
- *DONO* (NABUTO LS): wa.me/556993733829
+ * DONO * (COFS LINDO): wa.me/5511910592328
  
-
-
- ITACHI DO HYPE: wa.me/555599368698
-
- GUSTAVO: wa.me/5519994414618
-
  
- *CANAIS NO YOUTUBE* :
-
- *CANAL DO DONO* : https://youtube.com/channel/UCZEtf9AlsC2zsJQwrfW-44w
-
- *CANAL DO ITACHI* : https://youtube.com/channel/UC0zqqFaRFj4QdvCHkUzLHTA
-
-
- *CANAL DO GUSTAVO* : NÃO TEM❌
-
+ 
 
    [𝑨𝑳𝑮𝑼𝑵𝑺 𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺 𝑵𝑨̃𝑶 𝑬𝑺𝑻𝑨̃𝑶 𝑭𝑼𝑵𝑪𝑰𝑶𝑵𝑨𝑵𝑫𝑶, 𝑺𝑬 𝑨𝑪𝑯𝑨𝑹 𝑹𝑼𝑰𝑴 𝑬́ 𝑺𝑰𝑴𝑷𝑳𝑬𝑺 𝑷𝑨𝑹𝑨 𝑫𝑬 𝑼𝑺𝑨𝑹, 𝑮𝑹𝑨𝑻𝑶!]
-
-
-
  
     𝑩𝑬𝑴 𝑽𝑰𝑵𝑫𝑶 𝑨𝑶 𝑴𝑬𝑵𝑼 𝑫𝑶 𝑩𝑶𝑻🤭
-
-
-
  
-         【 $INÚTEIS* 】 :
-
-  ➡ ${prefix}blocklist
+         【$ INÚTEIS *】:
+  ➡ $ { prefix } lista de bloqueio
      【LISTA DE BLOCK】
-  ➡ ${prefix}chatlist
-     【LISTA CHAT】❌
-  ➡ ${prefix}ping
+  ➡ lista de bate- papo $ { prefix }
+     【LISTA CHAT】 ❌
+  ➡ $ { prefix } ping
      【PING】
-  ➡ ${prefix}bugreport
-     【REPORTA BUG】❌
-
-
+  ➡ Relatório de bug $ { prefix }
+     【REPORTA BUG】 ❌
       
-      *ZOAÇÃO* :
-
-
-  ➡ ${prefix}%GADO
+      * ZOAÇÃO *:
+  ➡ $ { prefix } % GADO
    
-  ➡ ${prefix}%CORNO(A)
-
-  ➡ ${prefix}%NAMORARESSEANO
-
-  ➡ ${prefix}%GAY
-
-
-
-
-    【 *MAIS USADOS RECENTEMENTE* 】
+  ➡ $ { prefix } % CORNO (A)
+  ➡ $ { prefix } % NAMORARESSEANO
+  ➡ $ { prefix } % GAY
+    【* MAIS USADOS RECENTEMENTE *】
   
-  ➡ ${prefix}sticker
+  ➡ $ { prefixo } fig
     【FAZER FIGURINHA】
-  ➡ ${prefix}toimg
+  ➡ $ { prefix } toimg
     【CONVERTE FIGURINHA EM IMAGEM】
-  ➡ ${prefix}tomp3
+  ➡ $ { prefix } tomp3
     【PEGAR ÁUDIO DE ALGUM VÍDEO】
-
  
-
-     【FOTOS ANIME/LOLI e etc】
+     【FOTOS ANIME / LOLI e etc】
  
-
-  ➡${prefix}loli
-  【foto de loli】
-
-  ➡${prefix}nekoanime
+  ➡ $ { prefix } loli
+  【Foto de loli】
+  ➡ $ { prefix } nekoanime
    【NEKO ANIME】
-
-  ➡${prefix}randomanime
-   【anime aleatório】
-
-
-  ➡${prefix}pokémon
-   【foto de pokémon】
-
-
+  ➡ $ { prefix } randomanime
+   【Anime aleatório】
+  ➡ $ { prefix } pokémon
+   【Foto de Pokémon】
    
-
-
      
-
-
-        *MELHORES SOBRE ÁUDIO*
+        * MELHORES SOBRE ÁUDIO *
  
-  ➡ ${prefix}play nome da música
-    【DOWNLOAD MÚSICA】
-  ➡ ${prefix}tts pt texto
+  ➡ $ { prefix } reproduzir nome da música
+    【BAIXAR MÚSICA】
+  ➡ $ { prefix } reproduzir musica em texto
     【FALAR SEU TEXTO】
-
-
-
-      +18 *NÃO USE NO GRUPO SUJEITO A BAN*
-
+      +18 * NÃO USE NO GRUPO SUJEITO A BAN *
   
-  ➡${prefix}hentai
-    【foto hentai】
-
-  ➡${prefix}randomhentaio
-    【random hentai】
-
-
-
-
-
-
+  ➡ $ { prefix } carlosgay
+    【Foto hentai】
+  ➡ $ { prefix } randomhentaio
+    【Hentai aleatório】
       
-
-         【 𝑶𝑼𝑻𝑹𝑶𝑺 】
+         【𝑶𝑼𝑻𝑹𝑶𝑺】
   
-  ➡ ${prefix}send
-  ➡ ${prefix}wame
-  ➡ ${prefix}exe
-  ➡ ${prefix}qrcode
-  ➡ ${prefix}afk
-  ➡ ${prefix}timer
-  ➡ ${prefix}fml
-  ➡ ${prefix}fml2
-
-
-
-
-
-
-
-          【 *FONTES* 】
-
-  ➡ ${prefix}bpink
-  ➡ ${prefix}marvellogo
-  ➡ ${prefix}snowwrite
-  ➡ ${prefix}3dtext
-  ➡ ${prefix}ninjalogo
-  ➡ ${prefix}water
-  ➡ ${prefix}firetext
-  ➡ ${prefix}logowolf
-  ➡ ${prefix}logowolf2
-  ➡ ${prefix}phlogo
-  ➡ ${prefix}glitch        𝑨 𝑴𝑨𝑰𝑶𝑹𝑰𝑨 𝑵𝑨̃𝑶 𝑷𝑬𝑮𝑨         
-  ➡ ${prefix}neonlogo
-  ➡ ${prefix}neonlogo2
-  ➡ ${prefix}lionlogo
-  ➡ ${prefix}jokerlogo
-  ➡ ${prefix}shadow
-  ➡ ${prefix}burnpaper
-  ➡ ${prefix}coffee
-  ➡ ${prefix}lovepaper
-  ➡ ${prefix}woodblock
-  ➡ ${prefix}qowheart
-  ➡ ${prefix}mutgrass
-  ➡ ${prefix}undergocean
-  ➡ ${prefix}woodenboards
-  ➡ ${prefix}wolfmetal
-  ➡ ${prefix}metalictglow
-  ➡ ${prefix}8bit
-  ➡ ${prefix}ttp
-  ➡ ${prefix}herrypotter
-  ➡ ${prefix}pubglogo
-  ➡ ${prefix}quotemaker
+  ➡ $ { prefix } enviar
+  ➡ $ { prefix } wame
+  ➡  $ { prefix } exe
+  ➡  $ { prefix } qrcode
+  ➡  $ { prefix } afk
+  ➡  $ { prefix } cronômetro
+  ➡  $ { prefix } fml
+  ➡ $ { prefix } fml2
+          【* FONTES *】
+  ➡  $ { prefix } bpink
+  ➡  $ { prefix } marvellogo
+  ➡  $ { prefix } snowwrite
+  ➡  $ { prefix } 3dtext
+  ➡  $ { prefix } ninjalogo
+  ➡  $ { prefix } água
+  ➡  $ { prefix } firetext
+  ➡  $ { prefix } logowolf
+  ➡  $ { prefix } logowolf2
+  ➡  $ { prefix } phlogo
+  ➡  $ { prefix } falha 𝑨 𝑴𝑨𝑰𝑶𝑹𝑰𝑨 𝑵𝑨̃𝑶 𝑷𝑬𝑮𝑨         
+  ➡  $ { prefix } neonlogo
+  ➡  $ { prefix } neonlogo2
+  ➡  $ { prefix } lionlogo
+  ➡  $ { prefix } jokerlogo
+  ➡  $ { prefix } sombra
+  ➡  $ { prefix } burnpaper
+  ➡  $ { prefix } café
+  ➡  $ { prefix } papel de amor
+  ➡  $ { prefix } woodblock
+  ➡  $ { prefix } qowheart
+  ➡ $ { prefix } mutgrass
+  ➡ $ { prefix } underocean
+  ➡ $ { prefix } tábuas de madeira
+  ➡ $ { prefix } metal de lobo
+  ➡ $ { prefix } metalictglow
+  ➡ $ { prefix } 8 bits
+  ➡ $ { prefix } ttp
+  ➡ $ { prefix } herrypotter
+  ➡ $ { prefix } pubglogo
+  ➡ $ { prefix } citações
         
-       【 *MEDIA* 】
+       【 *MEIOS DE COMUNICAÇÃO* 】
   
-
-  ➡ ${prefix}trendtwit
-  ➡ ${prefix}randomkpop
+  ➡ $ { prefix } trendtwit
+  ➡ $ { prefix } randomkpop
      【FOTOS ALEATÓRIA DE KPOP🤢】
-  ➡ ${prefix}ytsearch
+  ➡ $ { prefix } ytsearch
     【PESQUISA】
       
-
-
-      【  *EDUCAÇÃO* 】
+      【* EDUCAÇÃO *】
   
-  ➡ ${prefix}wiki
-  ➡ ${prefix}wikien
-  ➡ ${prefix}nulis
-  ➡ ${prefix}quotes
-  ➡ ${prefix}quotes2
-  ➡ ${prefix}artinama
-
-
-     【 *DOWNLOADER* 】
+  ➡ $ { prefixwiki de }
+  ➡ $ { prefix } wikien
+  ➡ $ { prefix } nulis
+  ➡ $ { prefixcitações de }
+  ➡ $ { prefix } aspas2
+  ➡ $ { prefix } artinama
+     【* DOWNLOADER *】
   
-  ➡ ${prefix}images
+  ➡ $ { prefix } imagens
     【IMAGENS ALEATÓRIA】
-  ➡ ${prefix}ytmp3
+  ➡ $ { prefix } ytmp3
     【MP3】
-  ➡ ${prefix}ytmp4
-    【DOWNLOAD MP4(VIDEO)】
-  ➡ ${prefix}tiktok
-  ➡ ${prefix}joox
+  ➡ $ { prefix } ytmp4
+    【BAIXAR MP4 (VÍDEO)】
+  ➡ $ { prefix } tiktok
+  ➡ $ { prefix } joox
          
-
-           *MEME*
+           * MEME *
       
-
-
-  ➡  ${prefix}meme
+  ➡   $ { prefix } meme
   ➡ 【MEME INDONÉSIO】
-  ➡  ${prefix}memeindo
+  ➡   $ { prefix } memeindo
   ➡ 【MEMEINDO INDONESIO】
  
-
-
-
-
-     【 *MÚSICA* 】
+     【* MÚSICA *】
   
-  ➡ ${prefix}lirik
-  ➡ ${prefix}chord
-
-
-
-       ➡ *ISLAM*
+  ➡ $ { prefix } letra
+  ➡ acorde $ { prefix }
+       ➡ * ISLAM *
   
-   ➡ ${prefix}quran
-
-
-
-
-
-       【 *STALK* 】
+   ➡ $ { prefix } quran
+       【* STALK *】
   
   
-  ➡ ${prefix}tiktokstalk
-     【stalkear tik tok】
-  ➡ ${prefix}igstalk
-
-
-
-
-
-        【 *WIBU* 】
-
+  ➡ $ { prefix } tiktokstalk
+     【Tok stalkear tik】
+  ➡ $ { prefix } igstalk
+        【* WIBU *】
   
-  ➡ ${prefix}neonime
-  ➡ ${prefix}pokemon
-  ➡ ${prefix}loli
-  ➡ ${prefix}waifu
-  ➡ ${prefix}randomanime
-  ➡ ${prefix}husbu
-  ➡ ${prefix}husbu2
-  ➡ ${prefix}wait
-  ➡ ${prefix}nekonime
-
-
-
-      【 *DIVERSÃO* 】
+  ➡ $ { prefix } neonime
+  ➡ $ { prefix } Pokémon
+  ➡ $ { prefix } loliemeupau
+  ➡ $ { prefix } waifu
+  ➡ $ { prefix } randomanime
+  ➡ $ { prefix } husbu
+  ➡ $ { prefix } husbu2
+  ➡ $ { prefix } espere
+  ➡ $ { prefix } nekonime
+      【* DIVERSÃO *】
   
-  ➡ ${prefix}alay
-  ➡ ${prefix}gantengcek
-  ➡ ${prefix}watak
-  ➡ ${prefix}hobby
-  ➡ ${prefix}game
-  ➡ ${prefix}bucin
-  ➡ ${prefix}trust
-  ➡ ${prefix}dare
-  ➡ ${prefix}simi
-
-
-
-
-
-         【 *INFORMAÇÃO* 】
+  ➡ $ { prefix } alay
+  ➡ $ { prefix } gantengcek
+  ➡ $ { prefix } watak
+  ➡ $ { prefix } hobby
+  ➡ $ { prefixo } jogo
+  ➡ $ { prefix } bucin
+  ➡ $ { prefix } trust
+  ➡ $ { prefix } ousar
+  ➡ $ { prefix } simi
+         【* INFORMAÇÃO *】
   
-  ➡ ${prefix}bahasa
-     【SABER TODOS CÓDIGO DE PAIS】
-  ➡ ${prefix}kodenegara
-  ➡ ${prefix}kbbi
-  ➡ ${prefix}fakta
-  ➡ ${prefix}infocuaca
-  ➡ ${prefix}infogempa
-  ➡ ${prefix}jadwaltvnow
-  ➡ ${prefix}covid
-
-
-          【 SÓ FUNCIONA COM O DONO 】
-
-
+  ➡ $ { prefix } bahasa
+     【SABRE TODOS CÓDIGO DE PAIS】
+  ➡ $ { prefix } kodenegara
+  ➡ $ { prefix } kbbi
+  ➡ $ { prefix } fakta
+  ➡ $ { prefix } infocuaca
+  ➡ $ { prefix } infogempa
+  ➡ $ { prefix } jadwaltvnow
+  ➡ $ { prefix } covid
+          【SÓ FUNCIONA COM O DONO】
   
-   ➭ ${prefix}setprefix
+   ➭ $ { prefix } setprefix
      【MUDAR PREFIXO】
-   ➭ ${prefix}block
+   ➭ bloco $ { prefix }
      【BLOQUEAR】
-   ➭ ${prefix}bc
+   ➭ $ { prefix } bc
      【TM】
-   ➭ ${prefix}bcgc
+   ➭ $ { prefix } bcgc
      【TM SÓ PARA MEMBROS DO GP】
-   ➭ ${prefix}clearall
+   ➭ $ { prefix } clearall
      【APAGAR TODAS MSG】
-
-`
-}
-
-exports.help = help
